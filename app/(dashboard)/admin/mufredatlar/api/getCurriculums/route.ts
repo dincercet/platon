5d67135898b06889134e8e7e34a8f7a7180d6883
udin <dincercetiner@hotmail.com>
2024-06-09 18:10:41 +0300

    checking related periods for curriculums

--- app/(dashboard)/admin/mufredatlar/api/getCurriculums/route.ts
+++ app/(dashboard)/admin/mufredatlar/api/getCurriculums/route.ts
@@ -18,7 +18,7 @@ export async function GET(): Promise<NextResponse> {
 
   //we will have to check if there are related periods
   //if there are any, we need to know on the client side, to show the appropriate button (make legacy or delete)
-  let relatedPeriods;
+  let relatedPeriods: { curriculum_id: number }[] | null;
   try {
     //find if there are related periods, if so get the curriculum ids
     relatedPeriods = await prisma.curriculum_periods.findMany({
@@ -43,17 +43,43 @@ export async function GET(): Promise<NextResponse> {
         course: { select: { name: true } },
         weeks: { select: { id: true, week_no: true, description: true } },
       },
+      orderBy: { legacy: "desc" },
     });
 
     //check if curriculums is null
-    if (!curriculums)
+    if (!curriculums) {
       return NextResponse.json(
         { error: "No curriculums found" },
         { status: 404 },
       );
-    //success
-    else
-      return NextResponse.json({ curriculums: curriculums }, { status: 200 });
+    } else {
+      //here we check each related period's curriculum_id and add a flag to each curriculum to show if it's related to a period
+
+      //add a flag to each curriculum
+      let bindedCurriculums = curriculums.map((curriculum) => {
+        return { ...curriculum, isRelated: false };
+      });
+      //within nested for loops
+      for (let i = 0; i < bindedCurriculums.length; i++) {
+        for (let j = 0; j < relatedPeriods.length; j++) {
+          //compare the ids
+          if (bindedCurriculums[i].id === relatedPeriods[j].curriculum_id) {
+            //if same, add flag and put it in the new array
+            bindedCurriculums[i].isRelated = true;
+
+            //remove the related id so we don't check the same id again
+            relatedPeriods.splice(j, 1);
+            break;
+          }
+        }
+      }
+
+      //success
+      return NextResponse.json(
+        { curriculums: bindedCurriculums },
+        { status: 200 },
+      );
+    }
   } catch (e) {
     //db error
     logger.error("prisma error fetching curriculums", e);
